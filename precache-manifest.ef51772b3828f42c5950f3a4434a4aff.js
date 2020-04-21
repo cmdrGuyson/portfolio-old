fc676336d59081097187dd0d3e213319dbf80bef
cmdrGuyson <gayangakuruppu@gmail.com>
2020-04-22 04:04:29 +0530

    Updates

---
+++ precache-manifest.ef51772b3828f42c5950f3a4434a4aff.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d41de2e4f6f980a1dda54b5553006a4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "88f5437e4b1cbb7c6650",
+    "url": "/portfolio/static/css/main.9b7776bd.chunk.css"
+  },
+  {
+    "revision": "1f85322b4e7fcc963af9",
+    "url": "/portfolio/static/js/2.5cb092bb.chunk.js"
+  },
+  {
+    "revision": "4d47ffde3dc69784e8e23fd910817ef8",
+    "url": "/portfolio/static/js/2.5cb092bb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88f5437e4b1cbb7c6650",
+    "url": "/portfolio/static/js/main.953466da.chunk.js"
+  },
+  {
+    "revision": "6c1d3ad26a766dc494c1",
+    "url": "/portfolio/static/js/runtime-main.d597d31e.js"
+  },
+  {
+    "revision": "d0e7c1aaea57a2ab7a50ac239d0753b9",
+    "url": "/portfolio/static/media/back.d0e7c1aa.png"
+  },
+  {
+    "revision": "32df319c201685d00d7f3f7d084e46ff",
+    "url": "/portfolio/static/media/logo.32df319c.png"
+  }
+]);
